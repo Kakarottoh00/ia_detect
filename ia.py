b145aefd8eb0475898901076961bb26e68ab37be
Rony Silva <92266292+Kakarottoh00@users.noreply.github.com>
2024-09-23 17:51:44 -0300

    Update ia.py
    so atualizei comentarios

--- ia.py
+++ ia.py
@@ -2,7 +2,7 @@ from vidgear.gears import CamGear
 import cv2
 from ultralytics import YOLO
 
-# Load the YOLOv8 model
+# aqui o yolo é chamado
 model = YOLO('yolov8n.pt')
 
 stream = CamGear(source='https://youtube.com/shorts/jq2-m29tRW4?si=oJxg3KMi1NHn2bO2', stream_mode=True, logging=True).start()
@@ -13,13 +13,13 @@ while True:
     if frame is None:
         break
 
-    # Run YOLOv8 inference on the frame
+    # aqui mostra a inferencia do yolo
     results = model(frame)
 
-    # Visualize the results on the frame
+    # aqui mostra os resultados
     annotated_frame = results[0].plot()
 
-    # Display the annotated frame
+    # aqui mostra o que o yolo detectou
     cv2.imshow("YOLOv8 Inference", annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
